d8be5e380b6a4f33a7112ecda3f196e12454a777
Enrique Diaz <enrique.diaz@alumnos.udg.mx>
2018-12-20 19:39:55 -0600

    Number Wizard Script Created
    Debug.Log(""); added

---
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Welcome to Number Wizard");
+		Debug.Log("Pick a Number");
+		Debug.Log("Higher number is: ");
+		Debug.Log("Lowest number is: ");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
