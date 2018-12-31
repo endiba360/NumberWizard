a7f1a99b2e8ba1abf4e087ee55998dcfc5b29c72
Enrique Diaz <enrique.diaz@alumnos.udg.mx>
2018-12-30 20:09:23 -0600

    Player input added
    Player input added using GetKeyDown structure

--- NumberWizard/Assets/Scripts/NumberWizard.cs
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
+	int max = 1000;
+	int min = 1;
+
 	// Use this for initialization
 	void Start () {
+	
 		Debug.Log("Welcome to Number Wizard");
-		Debug.Log("Think in an Number");
-		Debug.Log("Between 1,000 and 1");
+		Debug.Log("Think in a Number, do not tell me yet!");
+		Debug.Log("The min number you can think is " + min);
+		Debug.Log("The max number you can think is " + max);
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Debug.Log("You pressed Up!");
+	
+		}else if(Input.GetKeyDown(KeyCode.DownArrow)){		
+			Debug.Log("You pressed Down!");
+		
+		}else if(Input.GetKeyDown(KeyCode.Return)){
+			Debug.Log("You hit Enter!");
+
+		}
 	}
 }
