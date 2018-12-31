6027494d346357039941642c1de6c94d6f55ca92
Enrique Diaz <enrique.diaz@alumnos.udg.mx>
2018-12-30 20:41:06 -0600

    Variables Calculation
    We made a variables calculation in order to guess the number of the player reloading the guess variable so if gets higher or lower than our initial guess

--- NumberWizard/Assets/Scripts/NumberWizard.cs
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -6,14 +6,19 @@ public class NumberWizard : MonoBehaviour {
 
 	int max = 1000;
 	int min = 1;
+	int guess = 500;
 
 	// Use this for initialization
 	void Start () {
 	
 		Debug.Log("Welcome to Number Wizard");
-		Debug.Log("Think in a Number, do not tell me yet!");
+		Debug.Log("Think in a Number, do not tell me yet!!!!");
 		Debug.Log("The min number you can think is " + min);
 		Debug.Log("The max number you can think is " + max);
+		Debug.Log("Push UP if it's higher, DOWN if lower, ENTER if I got the right number :)");
+		Debug.Log("---------------------------------------");
+		Debug.Log("Did you think in... "+ guess);
+		max = max + 1;
 	
 	}
 	
@@ -22,13 +27,17 @@ public class NumberWizard : MonoBehaviour {
 		
 		if(Input.GetKeyDown(KeyCode.UpArrow))
 		{
-			Debug.Log("You pressed Up!");
+			min = guess;
+			guess = (max+min) / 2;
+			Debug.Log("Did you think in... "+ guess);
 	
 		}else if(Input.GetKeyDown(KeyCode.DownArrow)){		
-			Debug.Log("You pressed Down!");
+			max = guess;
+			guess = (max + min) / 2;
+			Debug.Log("Did you think in... "+ guess);
 		
 		}else if(Input.GetKeyDown(KeyCode.Return)){
-			Debug.Log("You hit Enter!");
+			Debug.Log("I got it!!! Your number is "+ guess);
 
 		}
 	}
